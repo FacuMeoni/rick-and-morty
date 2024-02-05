62eb8672f47780301cab4c692a06c876cbb23603
FacuMeoni <meonifacu@gmail.com>
2024-02-05 17:32:26 -0300

    Finish login and register section

--- Client/scripts/script-register.js
+++ Client/scripts/script-register.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const registerForm = document.querySelector('#register-form');
     const errorText = document.createElement('p');
     const registerButton = document.querySelector('#register-button');
+    const inputIcon = document.querySelector("#input-icon");
     errorText.classList.add('error-text');
     inputUsername.addEventListener('input', checkEmptyValue);
     inputPassword.addEventListener('input', checkEmptyValue);
@@ -57,6 +58,23 @@ document.addEventListener('DOMContentLoaded', () => {
       const userValue = inputUsername.value;
       
       await getRegister(userValue, passValue); // Esperar la respuesta de getLogging
+    });
+
+    inputIcon.addEventListener("click", (e) => {
+      e.preventDefault();
+    
+      inputIcon.setAttribute(
+        'src',
+        inputPassword.getAttribute('type') === 'password' ?
+        '../../assets/eye-off.svg' 
+         :
+        "../../assets/eye.svg" 
+      )
+    
+      inputPassword.setAttribute(
+        "type",
+        inputPassword.getAttribute("type") === "password" ? "text" : "password"
+      );
     });
   
     checkEmptyValue();
